90403d7005ac122a8bfb3c7e26cd78ed2bd7a02e
MaxProvin <maxprovin01@gmail.com>
2016-09-01 00:34:42 +0100

    links to categories implemented, added dotenv gem

--- clothing_site/spec/features/homepage_feature_spec.rb
+++ clothing_site/spec/features/homepage_feature_spec.rb
@@ -1,20 +1,30 @@
 feature 'Landing Page' do
 
-	context 'User not signed in' do
-		scenario 'Landing Page should not redirect to user homepage' do 
+	context 'Categories' do
+		scenario 'there should be 6 total'do
+			visit '/'
+			expect(page).to have_link 'Women’s footwear'
+			expect(page).to have_link 'Men’s footwear'
+			expect(page).to have_link 'Women’s casualwear'
+			expect(page).to have_link 'Men’s casualwear'
+			expect(page).to have_link 'Women’s formalwear'
+			expect(page).to have_link 'Men’s formalwear'
+		end
+	end
+
+	context 'when user is not signed in' do
+		scenario 'should not redirect to user homepage' do 
 			visit '/'
 			expect(current_path).to eq '/'
 		end
 	end
 
-	context 'User signed in' do
-
+	context 'when user is signed in' do
 		before do
 			sign_user_up
 		end
 
-		scenario 'Landing Page should direct to user homepage' do 
-			visit '/'
+		scenario 'should direct to user homepage' do 
 			expect(current_path).to eq '/home'
 		end
 	end
