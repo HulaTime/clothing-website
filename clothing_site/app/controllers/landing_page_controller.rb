678cf7e60edee6833e224a3ce8966943cff5b249
MaxProvin <maxprovin01@gmail.com>
2016-09-01 00:07:06 +0100

    added tests for user authentication

--- clothing_site/app/controllers/landing_page_controller.rb
+++ clothing_site/app/controllers/landing_page_controller.rb
@@ -1,6 +1,7 @@
 class LandingPageController < ApplicationController
 
 	def index
+		redirect_to home_path
 	end
 
 end
--- clothing_site/spec/features/homepage_feature_spec.rb
+++ clothing_site/spec/features/homepage_feature_spec.rb
@@ -1,7 +1,19 @@
 feature 'Landing Page' do
 
 	context 'User not signed in' do
-		scenario 'Landing Page should direct to homepage' do 
+		scenario 'Landing Page should not redirect to user homepage' do 
+			visit '/'
+			expect(current_path).to eq '/'
+		end
+	end
+
+	context 'User signed in' do
+
+		before do
+			sign_user_up
+		end
+
+		scenario 'Landing Page should direct to user homepage' do 
 			visit '/'
 			expect(current_path).to eq '/home'
 		end
